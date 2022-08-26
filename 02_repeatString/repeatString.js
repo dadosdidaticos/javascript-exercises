f5657b98a5056e68d0491e02beac9aa4781c5f42
dadosdidaticos <marcosvcc@poli.ufrj.br>
2022-08-26 00:12:38 -0300

    Exercícios 1 a 3 feitos

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let repeating = ""
+    if (num<0){
+        return 'ERROR'
+    }else{
+        for (let i=0;i<num;i++) {
+            repeating += string
+        }
+        return repeating
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(text='') {
+    textArray = text.split("");
+    let reversed = ""
+    let index = textArray.length-1
+    while (index>=0){
+        reversed+=textArray[index]
+        index--
+    }
+    return reversed
 };
 
+
+console.log(reverseString(('hello')))
 // Do not edit below this line
 module.exports = reverseString;
