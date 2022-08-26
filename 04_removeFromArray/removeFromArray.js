6cbf2b1c05eb8510d37f1823f676bb2b40ed23c5
dadosdidaticos <marcosvcc@poli.ufrj.br>
2022-08-26 09:31:50 -0300

    Exercício 4 feito

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray,...myValues) {
+    let value
+    for (value of myValues){
+        for (item of myArray){
+            if (value===item){
+                myArray.splice(myArray.indexOf(item),1)
+            }
+        }
 
+    }
+    return myArray
 };
-
+console.log(removeFromArray([1,2,3],3))
 // Do not edit below this line
 module.exports = removeFromArray;
+
