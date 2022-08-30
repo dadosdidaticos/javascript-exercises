bf4ca448b84adf55587c5e54e4430665e06a5289
dadosdidaticos <marcosvcc@poli.ufrj.br>
2022-08-30 10:20:55 -0300

    Exercícios 11 a 12 feitos

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let peopleAges = []
+    people.map(function(person){
+        if (person.yearOfDeath != null){
+            peopleAges.push(person.yearOfDeath-person.yearOfBirth)
+        }else{
+            const today = new Date();
+            peopleAges.push(today.getFullYear()-person.yearOfBirth)
+        }
+        return peopleAges
+    });
+    let maxAge = Math.max.apply(null,peopleAges)
+    let indexMaxAge = peopleAges.indexOf(maxAge)
+    return people[indexMaxAge]
 };
 
 // Do not edit below this line
